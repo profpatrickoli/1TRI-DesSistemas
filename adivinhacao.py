327f9ff2ead1db33b5e368313739402c7083291a
Patrick <patrick.staroin@escola.pr.gov.br>
2026-03-02 09:49:00 -0300

    Aula 02-03

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,19 +1,25 @@
 # importação da biblioteca
 import random
-# sorteio do número aleatório
-numero = random.randint(0,10)
-# TESTE
-#print(numero)
+
+# Configurações do jogo
 tentativas = 1
-while (tentativas <= 3):
+errou = True
+sorteio_max = 10
+tentativas_max = 3
+numero = random.randint(0,sorteio_max)
+
+
+while (tentativas <= tentativas_max):
     print("Tentativa:", tentativas)
     chute = int(input("Digite o seu chute (0 a 10):"))
     if chute == numero:
         print("Parabéns, você é o bonzão mesmo")
+        errou = False
         break
     else:
         print("Errou :c")
     tentativas = tentativas + 1
+    
+if errou == True:
+    print("O número sorteado era:", numero) # mostra p quem errou
 print("### FIM DO JOGO ###")
-# Exercício: caso a pessoa erre todas as tentativas,
-# mostre o número sorteado ao final do jogo
