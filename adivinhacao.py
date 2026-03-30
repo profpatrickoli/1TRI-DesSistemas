9046a28098323a98893347ff05dd1ee67b0a4083
Patrick <patrick.staroin@escola.pr.gov.br>
2026-03-30 09:25:51 -0300

    Criado o menu jogos.py, alteração no adivinhacao e no forca

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,25 +1,29 @@
 # importação da biblioteca
 import random
 
-# Configurações do jogo
-tentativas = 1
-errou = True
-sorteio_max = 10
-tentativas_max = 3
-numero = random.randint(0,sorteio_max)
+def jogar():
+    # Configurações do jogo
+    tentativas = 1
+    errou = True
+    sorteio_max = 10
+    tentativas_max = 3
+    numero = random.randint(0,sorteio_max)
 
 
-while (tentativas <= tentativas_max):
-    print("Tentativa:", tentativas)
-    chute = int(input("Digite o seu chute (0 a 10):"))
-    if chute == numero:
-        print("Parabéns, você é o bonzão mesmo")
-        errou = False
-        break
-    else:
-        print("Errou :c")
-    tentativas = tentativas + 1
-    
-if errou == True:
-    print("O número sorteado era:", numero) # mostra p quem errou
-print("### FIM DO JOGO ###")
+    while (tentativas <= tentativas_max):
+        print("Tentativa:", tentativas)
+        chute = int(input("Digite o seu chute (0 a 10):"))
+        if chute == numero:
+            print("Parabéns, você é o bonzão mesmo")
+            errou = False
+            break
+        else:
+            print("Errou :c")
+        tentativas = tentativas + 1
+        
+    if errou == True:
+        print("O número sorteado era:", numero) # mostra p quem errou
+    print("### FIM DO JOGO ###")
+
+if (__name__ == "__main__"):
+    jogar()
